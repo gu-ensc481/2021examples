539a92b81cc7f39b1df8036ffa931a9fc7d5b3e1
tim <fitzgeraldt@gonzaga.edu>
2021-01-19 11:36:14 -0800

    prep for class

---
+++ class-demos/01-19/main.m
@@ -0,0 +1,32 @@
+%% In Class: Types in Matlab
+% T. Fitzgerald, 19-Jan-2021
+
+clear variables
+close all
+clc
+
+%% Numerbers default type
+
+
+%% Arrays of numbers
+
+
+%% Character arrays
+
+
+%% Strings
+% these are newer to Matlab (from 2016b) and mostly look and walk like
+% character arrays.  See the website for FAQ on differences between strings
+% and chars
+% https://www.mathworks.com/help/matlab/matlab_prog/frequently-asked-questions-about-string-arrays.html
+
+s1 = "This is a string";
+
+
+
+%% Structures
+% very useful named fields
+
+%% Cells
+% Like a list that can contain anything
+
