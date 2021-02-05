3d4a29866835e7d33ee87666d947579e3534b75e
tim <fitzgeraldt@gonzaga.edu>
2021-02-05 00:18:14 -0800

    rando matlab files from class

---
+++ class-demos/02-04/f.m
@@ -0,0 +1,11 @@
+function y = f(x)
+y = secfcn(x);
+
+end
+
+
+function z = secfcn(y)
+
+z = y + 1;
+
+end
---
+++ class-demos/02-04/main.m
@@ -0,0 +1,27 @@
+%% Problem 1
+% Problem statement/summary.
+
+clear variables
+close all
+clc
+
+%%
+a = 1;
+plot(rand(10))
+
+%% Heading
+% rando text
+
+b = sin(30*pi/180)
+
+
+%%
+% This is more text
+
+%%
+f(2)
+
+%%
+function y = f(x)
+    y = x^2;
+end
