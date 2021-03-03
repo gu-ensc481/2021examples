4ab679efde76fc3ea1c392099625e3a0e7d6e928
tim <fitzgeraldt@gonzaga.edu>
2021-03-02 18:46:58 -0800

    add more stuff

--- class-demos/03-02/gs.jl
+++ class-demos/03-02/gs.jl
@@ -1,32 +1,7 @@
-##
-using LinearAlgebra
-
-nx = 5
-ny = nx
-
-T_south = 0
-T_east  = 50
-T_north = 100
-T_west  = 75
-
-
-T0 = zeros(nx,ny)
-
-## Apply BC
-T0[:,1]   .= T_south
-T0[end,:] .= T_east
-T0[:,end] .= T_north
-T0[1,:]   .= T_west
-
-##
-
-tol = 1e-6
-maxIter = 1000
-
-function jacobi(nx,ny, tol, maxIter, T0)
+## 
+function gaussSeidel(nx,ny, tol, maxIter, T0)
 
     T = copy(T0)
-    Tnew = copy(T0)
     residuals = zeros(nx,ny)
 
     flag = 0
@@ -37,14 +12,13 @@ function jacobi(nx,ny, tol, maxIter, T0)
         # update all the open values of T
         # T[i,j] = 1/4*( T[i+1,j] + T[i-1,j] + T[i,j+1] + T[i,j-1]  )
 
+        # Gauss-Seidel (no Tnew)
         for i = 2:nx-1
             for j = 2:ny-1
-                Tnew[i,j] = 1/4*( T[i+1,j] + T[i-1,j] + T[i,j+1] + T[i,j-1]  )
+                T[i,j] = 1/4*( T[i+1,j] + T[i-1,j] + T[i,j+1] + T[i,j-1]  )
             end
         end
-        T = copy(Tnew)
-
-
+        
         for i = 2:nx-1
             for j = 2:ny-1
                 residuals[i,j] = T[i+1,j] + T[i-1,j] + T[i,j+1] + T[i,j-1] - 4*T[i,j]
@@ -64,9 +38,5 @@ function jacobi(nx,ny, tol, maxIter, T0)
 end
 
 ##
-T, iter = jacobi(nx,ny,tol, maxIter, T0)
-
-
-##
-include("gs.jl")
 T, iter = gaussSeidel(nx,ny,tol, maxIter, T0)
+
