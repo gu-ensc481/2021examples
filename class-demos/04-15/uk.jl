df9776375d0cbccb7b4b942fdf8ee7150ed8ad2a
tim <fitzgeraldt@gonzaga.edu>
2021-04-15 21:42:43 -0700

    cleanup

--- class-demos/04-15/uk.jl
+++ class-demos/04-15/uk.jl
@@ -4,7 +4,7 @@ Pkg.activate(".")
 
 using ForwardDiff, LinearAlgebra, DifferentialEquations
 
-##
+## Setup the system
 m = 1.
 g = 9.81
 l = 0.5
@@ -13,11 +13,11 @@ M = diagm(0=>[m,m])
 Qex = [0, -m*g]
 ϕ(q) = -l^2 + q[1]^2 + q[2]^2
 
-##
+## Built UK equations
 A(q) = ForwardDiff.gradient( ϕ, q)'
 b(q,qd) = -qd'*ForwardDiff.hessian(ϕ,q)*qd
 
-p = (M, Qex, A,b )
+p = (M, Qex, A, b )
 function sysode!(dz, z, p, t)
     M, Qex, A, b = p
     n = size(M,1)
@@ -31,7 +31,7 @@ function sysode!(dz, z, p, t)
     An = A(q)
     bn = b(q,qd)
 
-    qddot = a + Ms\(  (An*inv(Ms))\( bn - An*a)  )
+    qddot = a + Ms\( (An/Ms)\( bn - An*a ) )
 
     dz[1:n] .= qd
     dz[n+1:2*n] .= qddot
@@ -41,4 +41,4 @@ end
 ## check to make sure that the syntax is working
 zd = zeros(4,1)
 sysode!(zd, [1,2,3,4], p, 0)
-
+zd
