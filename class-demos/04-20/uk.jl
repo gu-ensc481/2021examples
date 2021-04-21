f049df64bae858d2c61cb88397b3e1d3bbd5ad1c
tim <fitzgeraldt@gonzaga.edu>
2021-04-21 13:20:52 -0700

    live from class

--- class-demos/04-20/uk.jl
+++ class-demos/04-20/uk.jl
@@ -3,6 +3,7 @@ using Pkg
 Pkg.activate(".")
 
 using ForwardDiff, LinearAlgebra, DifferentialEquations
+using Plots
 
 ## Setup the system
 m = 1.
@@ -42,3 +43,28 @@ end
 zd = zeros(4,1)
 sysode!(zd, [1,2,3,4], p, 0)
 zd
+
+## z = [x, y, xdot, ydot]
+tspan = (0, 30.)
+θ0 = 30*pi/180
+x0 = l*sin(θ0)
+y0 = -l*cos(θ0)
+z0 = [x0, y0, 0, 0]
+prob = ODEProblem( sysode!, z0, tspan, p)
+##
+sol = solve( prob )
+
+#matlab: deval( sol, 1.3)
+#julia: sol(1.3)
+
+##
+x(t) = sol(t)[1]
+y(t) = sol(t)[2]
+
+plot( x, tspan[1], tspan[2], label="x [m]" )
+plot!( y, tspan[1], tspan[2], label="y [m]" , 
+    xlabel="Time t [s]", ylabel="Position [m]")
+
+##
+err(t) = (sqrt(x(t)^2 + y(t)^2) - l)/l
+plot( err, tspan[1], tspan[2])
